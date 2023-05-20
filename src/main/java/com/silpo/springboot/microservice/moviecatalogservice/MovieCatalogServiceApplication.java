52245a2dd70cd6599c5f68194c932459394016f0
silpayan.dutta <silpayan.dutta@workday.com>
2023-05-20 12:06:37 -0700

    WebClient, webflux and java version issue in pom.xml

--- src/main/java/com/silpo/springboot/microservice/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/silpo/springboot/microservice/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,9 +2,26 @@ package com.silpo.springboot.microservice.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+	//Rest Template for rest call create a bean out of it
+	//Beas in spring are Singleton
+	@Bean
+	public RestTemplate getRestTemplate(){
+		return new RestTemplate();
+	}
+
+	//Used in Spring Reactive programing or Asynchronous
+	@Bean
+	public WebClient.Builder getWebClientBuilder(){
+		return WebClient.builder();
+
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- src/main/java/com/silpo/springboot/microservice/moviecatalogservice/resource/MovieCatalogResources.java
+++ src/main/java/com/silpo/springboot/microservice/moviecatalogservice/resource/MovieCatalogResources.java
@@ -3,10 +3,12 @@ package com.silpo.springboot.microservice.moviecatalogservice.resource;
 import com.silpo.springboot.microservice.moviecatalogservice.model.CatalogItem;
 import com.silpo.springboot.microservice.moviecatalogservice.model.Movie;
 import com.silpo.springboot.microservice.moviecatalogservice.model.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -17,23 +19,43 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResources {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webclientBuilder;
+
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-
-        //Rest Template for rest call
-        RestTemplate restTemplate = new RestTemplate();
 
         //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/1234", Movie.class);
         //get all movie ids
         List<Rating> ratings = Arrays.asList(
                 new Rating("7354", "4"),
-            new Rating("46rf8", "3")
+                new Rating("46rf8", "3")
         );
 
         //for each movie id, call movie info service and get details
         //put all together
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class);
+            //Using RestTemplate
+            //Deprecating this
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/"+rating.getMovieId(), Movie.class); //getting the data from movie-info-service
+
+            //Using Webclient.Builder
+            //RestTemplate suppose to be deprecated in the future,
+            //So this should be used it is Reactive or asynchronous
+            /*Movie movie = webclientBuilder.build()
+                    .get()
+                    .uri("http://localhost:8082/movies/"+rating.getMovieId())
+                    .retrieve()
+                    //empty container will notify once gets data
+                    .bodyToMono(Movie.class)
+                    //waits until mono is getting the data
+                    .block();*/
+
+
+
             return new CatalogItem(movie.getName(), "trans - " + userId, rating.getRating());
         }).collect(Collectors.toList());
 
