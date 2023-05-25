7cf4380d9416084d164901d84ba4a616702a5695
silpayan.dutta <silpayan.dutta@workday.com>
2023-05-24 22:10:05 -0700

    -Added dependency in pom from annotation -still problem with  @EnableCircuitBreaker

--- src/main/java/com/silpo/springboot/microservice/moviecatalogservice/MovieCatalogServiceApplication.java
+++ src/main/java/com/silpo/springboot/microservice/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -4,15 +4,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
-//@EnableCircuitBreaker
-//@EnableHystrixDashboard
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
 public class MovieCatalogServiceApplication {
 
 	//https://github.com/koushikkothagal/spring-boot-microservices-workshop/
